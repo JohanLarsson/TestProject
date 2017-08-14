4dfe365fb9a733899ae73b69a0d7fec797b02cf9
JohanLarsson <johan_gavle1@hotmail.com>
2017-08-14 19:30:48 +0200

    Added class library.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary1
+{
+    using Microsoft.CodeAnalysis;
+
+    public class Class1
+    {
+        public static Solution CreateSolution()
+        {
+            return new AdhocWorkspace().CurrentSolution;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,12 @@
 ﻿namespace ConsoleApp1
 {
+    using ClassLibrary1;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var sln = Class1.CreateSolution();
         }
     }
 }
